02ed7b36f9a89c7e947aa0e51d06c4fcc9b7a494
Reese Kunz <reesekunz21@gmail.com>
2020-01-09 00:56:00 -0700

    making the moves

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.scss";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      {/* INTRO  */}
+      <div className="intro">
+        <h3 className="intro-name">Reese Kunz </h3>
+        <p className="intro-title">Full-Stack Web Developer </p>
+        <p className="intro-tech-stack">JavaScript | React | Node | Express</p>
+      </div>
+      {/* MAIN CONTENT  */}
+      <div className="main-content-container">Hi durr</div>
+    </div>
+  );
+}
+
+export default App;

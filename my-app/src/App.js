e704a2659666b957b5e2b8b7949b6fbbae1a1fd4
Reese Kunz <reesekunz21@gmail.com>
2020-01-12 15:26:12 -0700

    added google analytics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,16 @@ import HireMePlz from "./components/HireMePlz/HireMePlz";
 import Footer from "./components/Footer/Footer";
 import Favicon16 from "./images/favicon-16x16.png";
 import Favicon32 from "./images/favicon-32x32.png";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 function App() {
   return (
     <div className="App">

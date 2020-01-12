26367ed50a59c9a93b320572c5681091498967d8
Reese Kunz <reesekunz21@gmail.com>
2020-01-12 15:35:01 -0700

    fixing errorz

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,20 +15,20 @@ import ReactGA from "react-ga";
 import auth from "./auth.ts"; // Sample authentication provider
 import { createBrowserHistory } from "history";
 
-const trackingId = "UA-156067052-1"; // Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-ReactGA.set({
-  userId: auth.currentUserId()
-  // any data that is relevant to the user session
-  // that you would like to track with google analytics
-});
+// const trackingId = "UA-156067052-1"; // Google Analytics tracking ID
+// ReactGA.initialize(trackingId);
+// ReactGA.set({
+//   userId: auth.currentUserId()
+// any data that is relevant to the user session
+// that you would like to track with google analytics
+// });
 
-const history = createBrowserHistory();
+// const history = createBrowserHistory();
 // Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
+// history.listen(location => {
+//   ReactGA.set({ page: location.pathname }); // Update the user's current page
+//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+// });
 
 function App() {
   return (

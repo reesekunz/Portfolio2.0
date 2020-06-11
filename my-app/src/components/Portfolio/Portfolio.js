ae014c8607ed3a1a290679aad1b9f003d71ff065
Reese Kunz <reesekunz21@gmail.com>
2020-06-11 04:06:24 -0700

    projects section styling

--- my-app/src/components/Portfolio/Portfolio.js
+++ my-app/src/components/Portfolio/Portfolio.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Portfolio.scss";
-// import projectInfo from "./ProjectInfo";
 import AdviceGiver from "../../images/AdviceGiver.png";
 import RandomActs from "../../images/RandomActs2.png";
 import CoordinatedStories from "../../images/CoordinatedStories.png";
 import WellDone from "../../images/WellDoneScreenshot.png";
-// import { IoIosInformationCircleOutline } from "react-icons/io";
+import { IoIosInformationCircleOutline, IoLogoGithub } from "react-icons/io";
+
 function Portfolio() {
   return (
     <div className="portfolio-container" id="portfolio">
@@ -15,41 +15,86 @@ function Portfolio() {
       <div className="project-container">
         <div className="row">
           <div className="project">
-            <img src={AdviceGiver} alt="Advice Giver project screenshot" />
-            <h3 className="project-name">Advice Giver</h3>
-            <p className="tech-stack">HTML, CSS, JavaScript</p>
+            <a
+              href="https://infallible-lumiere-c93889.netlify.com/"
+              alt="project-link"
+              target="_blank"
+              className="project-link"
+            >
+              <img src={AdviceGiver} alt="Advice Giver project screenshot" />
+            </a>
+            <div className="linkz">
+              <a
+                className="project-btn-link"
+                target="_blank"
+                href="https://github.com/advice-giver/Web-UI/pull/1/commits"
+              >
+                <IoLogoGithub size={40} className="icon" />
+              </a>
+              <h3 className="project-name">
+                <a
+                  href="https://infallible-lumiere-c93889.netlify.com/"
+                  alt="project-link"
+                  target="_blank"
+                  className="project-link"
+                >
+                  Advice Giver
+                </a>
+              </h3>
+              <div className="project-btn-link"></div>
+              {/* <a className="project-btn-link" target="_blank" href="#">
+                <IoIosInformationCircleOutline size={40} />
+              </a> */}
+            </div>
             <p className="project-description">
-              A marketing landing page for Advice Giver, a platform connecting
+              A responsive landing page for Advice Giver, a platform connecting
               people seeking advice with those willing to provide it.
-            </p>
-            <div className="linkz">
-              <a href="https://github.com/advice-giver/Web-UI/pull/1/commits">
-                Code
-              </a>
-              <a href="https://infallible-lumiere-c93889.netlify.com/">
-                Project
-              </a>
-            </div>
+            </p>{" "}
+            <p className="tech-stack">Tech Stack: HTML, LESS, JavaScript</p>
           </div>
+
           <div className="project">
-            <img
-              src={RandomActs}
-              alt="Random Acts Generator project screenshot"
-            />
-
-            <h3 className="project-name">Random Acts Generator</h3>
-            <p className="tech-stack">JavaScript, React</p>
-            <p className="project-description">
-              Worked with a front-end team of four. Random acts of kindness are
-              generated for the user to perform based on their added contacts.
-            </p>
-
+            <a
+              href="https://lucid-panini-415838.netlify.com/"
+              alt="project-link"
+              target="_blank"
+              className="project-link"
+            >
+              <img
+                src={RandomActs}
+                alt="Random Acts Generator project screenshot"
+              />
+            </a>
             <div className="linkz">
-              <a href="https://github.com/buildweek-random-acts-generator/front-end">
-                Code
+              <a
+                href="https://github.com/buildweek-random-acts-generator/front-end"
+                className="project-btn-link"
+                target="_blank"
+              >
+                <IoLogoGithub size={40} className="icon" />
               </a>
-              <a href="https://lucid-panini-415838.netlify.com/">Project</a>
+              <h3 className="project-name">
+                <a
+                  href="https://lucid-panini-415838.netlify.com/"
+                  alt="project-link"
+                  target="_blank"
+                  className="project-link"
+                >
+                  Random Acts Generator
+                </a>
+              </h3>
+              <div className="project-btn-link"></div>
+
+              {/* <a className="project-btn-link" target="_blank" href="#">
+                <IoIosInformationCircleOutline size={40} />
+              </a> */}
             </div>
+
+            <p className="project-description">
+              Random acts of kindness are generated for the user to perform
+              based on their added contacts.
+            </p>
+            <p className="tech-stack">Tech Stack: JavaScript, React</p>
           </div>
         </div>
         <div className="row">
@@ -58,37 +103,82 @@ function Portfolio() {
               src={CoordinatedStories}
               alt="Coordinated Stories project screenshot"
             />
-
-            <h3 className="project-name">Coordinated Story Telling</h3>
-            <p className="tech-stack">Node, Express</p>
-            <p className="project-description">
-              Our team's goal was to create a central location for gathering
-              success stories for the Bountiful Children's Foundation, showing
-              the positive impact donations have on the lives of children.
-            </p>
-
             <div className="linkz">
-              <a href="https://github.com/Coordinated-Story-Telling/Back-End">
-                Code
+              <a
+                href="https://github.com/Coordinated-Story-Telling/Back-End"
+                className="project-btn-link"
+                target="_blank"
+              >
+                <IoLogoGithub size={40} className="icon" />
               </a>
-              <a href="https://bountifulchildren.org/">Company</a>
+              <h3 className="project-name">
+                {/* <a
+                  href="https://github.com/Coordinated-Story-Telling/Back-End"
+                  alt="project-link"
+                  target="_blank"
+                  className="project-link"
+                > */}
+                Coordinated Story Telling
+                {/* </a> */}
+              </h3>
+              <div className="project-btn-link"></div>
+
+              {/* <a className="project-btn-link" target="_blank" href="#">
+                <IoIosInformationCircleOutline size={40} />
+              </a> */}
             </div>
+
+            <p className="project-description">
+              Created a central location for gathering success stories from the{" "}
+              {""}
+              <a href="https://bountifulchildren.org/">
+                Bountiful Children's Foundation
+              </a>
+              , showing the positive impact donations have had on the lives of
+              children.
+            </p>
+            <p className="tech-stack">Tech Stack: Node, Express</p>
           </div>
           <div className="project" id="row2">
-            <img src={WellDone} alt="WellDone project screenshot" />
-            <h3 className="project-name">WellDone</h3>
-            <p className="tech-stack">JavaScript, React, Node, Express</p>
-            <p className="project-description">
-              WellDone is a non-profit that provides clean and sustainable water
-              to areas in need. Built a BE to store well sensor data and a FE to
-              visualize which pumps are functioning or need maintenance.
-            </p>
-
+            <a
+              href="https://welldone-water.now.sh/"
+              alt="project-link"
+              target="_blank"
+              className="project-link"
+            >
+              <img src={WellDone} alt="WellDone project screenshot" />
+            </a>
             <div className="linkz">
-              <a href="https://github.com/labs17-WellDone">Code</a>
-              {/* <a href="#">Project</a> */}
-              <a href="https://welldone.org/">Company</a>
+              <a
+                href="https://github.com/labs17-WellDone"
+                className="project-btn-link"
+                target="_blank"
+              >
+                {" "}
+                <IoLogoGithub size={40} className="icon" />
+              </a>
+              <h3 className="project-name">
+                <a
+                  href="https://welldone-water.now.sh/"
+                  alt="project-link"
+                  target="_blank"
+                  className="project-link"
+                >
+                  WellDone
+                </a>
+              </h3>{" "}
+              <div className="project-btn-link"></div>
+              {/* <a className="project-btn-link" target="_blank" href="#">
+                <IoIosInformationCircleOutline size={40} />
+              </a> */}
             </div>
+
+            <p className="project-description">
+              Built a backend to persist well sensor data and a frontend to
+              visualize which pumps need maintenance for{" "}
+              <a href="https://welldone.org/">WellDone</a>.
+            </p>
+            <p className="tech-stack">Tech Stack: React, Node, Express</p>
           </div>
         </div>
       </div>

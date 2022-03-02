2c2d8235afe1590427ee616c4754a833a3b14a05
Reese Kunz <reesekunz21@gmail.com>
2022-03-02 11:32:33 -0800

    added blog section content

--- my-app/src/components/Blog/Blog.js
+++ my-app/src/components/Blog/Blog.js
@@ -8,7 +8,14 @@ function Blog() {
       <div className="section-header">
         <span>Blog</span>
       </div>
-      🚧 Coming soon 🚧 
+    <div className='blog-links'>
+      <a className='blog-link' href="https://medium.com/@reesek/gemint-product-updates-march-2-2022-92f815bea944?source=user_profile---------0-------------------------------">
+      Gemint Product Updates — March 2, 2022
+      </a>
+      <div id='view-all-wrapper'>
+      <a id='view-all-link' href="https://medium.com/@reesek">View All</a>
+      </div>
+      </div>
     </div>
   );
 }
